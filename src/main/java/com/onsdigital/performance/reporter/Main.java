8cf4bde1bba5137f3c0eadcd38717549ca8d0ce1
CarlHembrough <carlhuk@gmail.com>
2016-08-10 09:57:52 +0100

    Merged local change

--- src/main/java/com/onsdigital/performance/reporter/Main.java
+++ src/main/java/com/onsdigital/performance/reporter/Main.java
@@ -1,9 +1,6 @@
 package com.onsdigital.performance.reporter;
 
-import com.onsdigital.performance.reporter.google.GoogleAnalyticsProvider;
-import com.onsdigital.performance.reporter.influxdb.InfluxDbMetricsProvider;
 import com.onsdigital.performance.reporter.interfaces.FileUploader;
-import com.onsdigital.performance.reporter.interfaces.MetricsProvider;
 import com.onsdigital.performance.reporter.interfaces.ResponseTimeProvider;
 import com.onsdigital.performance.reporter.model.Metrics;
 import com.onsdigital.performance.reporter.model.ResponseTimes;
@@ -22,19 +19,20 @@ public class Main {
 
         log.debug("Starting performance reporter.");
 
-        log.debug("Gathering analytics data from Google.");
-        Metrics analytics = new GoogleAnalyticsProvider().getAnalytics();
-        fileUploader.uploadJsonForObject(analytics, "analytics.json");
+//        log.debug("Gathering analytics data from Google.");
+//        Metrics analytics = new GoogleAnalyticsProvider().getAnalytics();
+//        fileUploader.uploadJsonForObject(analytics, "analytics.json");
 
         log.debug("Gathering response times from Pingdom.");
         ResponseTimeProvider responseTimeProvider = new PingdomResponseTimeProvider();
-        ResponseTimes responseTimes = responseTimeProvider.getResponseTimes("Website (EN) CloudFlare");
-        fileUploader.uploadJsonForObject(responseTimes, "responsetimes.json");
 
-        log.debug("Gathering metrics from InfluxDB.");
-        MetricsProvider metricsProvider = new InfluxDbMetricsProvider();
-        Metrics metrics = metricsProvider.getMetrics("aTimeSeries", "cpu");
-        fileUploader.uploadJsonForObject(metrics, "metrics.json");
+        ResponseTimes metrics = responseTimeProvider.getResponseTimes("");
+        fileUploader.uploadJsonForObject(metrics, "responsetimes.json");
+
+//        log.debug("Gathering metrics from InfluxDB.");
+//        MetricsProvider metricsProvider = new InfluxDbMetricsProvider();
+//        Metrics metrics = metricsProvider.getMetrics("aTimeSeries", "cpu");
+//        fileUploader.uploadJsonForObject(metrics, "metrics.json");
 
         log.debug("Finished");
     }
