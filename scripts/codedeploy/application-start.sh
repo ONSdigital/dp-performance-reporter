51913dc24a51a8313b6135236b9fec866561bc67
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-08-12 07:19:19 +0100

    Remove unnecessary trailing whitespace

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -10,16 +10,16 @@ CONFIG=$(aws --region $AWS_REGION ec2 describe-tags --filters "Name=resource-id,
 
 (aws s3 cp s3://$CONFIG_BUCKET/performance-reporter/$CONFIG.asc . && gpg --decrypt $CONFIG.asc > $CONFIG) || exit $?
 
-source $CONFIG && docker run -d                                \
-  --env=AWS_BUCKET_NAME=$PERFORMANCE_BUCKET                    \
-  --env=GOOGLE_ACCOUNT_ID=$GOOGLE_ACCOUNT_ID                   \
-  --env=GOOGLE_PRIVATE_KEY=$GOOGLE_PRIVATE_KEY                 \
-  --env=GOOGLE_PROFILE_ID=$GOOGLE_PROFILE_ID                   \
-  --env=INFLUXDB_URL=$INFLUXDB_URL                             \
-  --env=PINGDOM_APPLICATION_KEY=$PINGDOM_APPLICATION_KEY       \
-  --env=PINGDOM_PASSWORD=$PINGDOM_PASSWORD                     \
-  --env=PINGDOM_USERNAME=$PINGDOM_USERNAME                     \
-  --name=performance-reporter                                  \
-  --net=influx                                                 \
-  --restart=always                                             \
+source $CONFIG && docker run -d                          \
+  --env=AWS_BUCKET_NAME=$PERFORMANCE_BUCKET              \
+  --env=GOOGLE_ACCOUNT_ID=$GOOGLE_ACCOUNT_ID             \
+  --env=GOOGLE_PRIVATE_KEY=$GOOGLE_PRIVATE_KEY           \
+  --env=GOOGLE_PROFILE_ID=$GOOGLE_PROFILE_ID             \
+  --env=INFLUXDB_URL=$INFLUXDB_URL                       \
+  --env=PINGDOM_APPLICATION_KEY=$PINGDOM_APPLICATION_KEY \
+  --env=PINGDOM_PASSWORD=$PINGDOM_PASSWORD               \
+  --env=PINGDOM_USERNAME=$PINGDOM_USERNAME               \
+  --name=performance-reporter                            \
+  --net=influx                                           \
+  --restart=always                                       \
   $ECR_REPOSITORY_URI/performance-reporter:$GIT_COMMIT
