40c99b72ebca09ea6362cf3882785e9c8fe247f5
Lloyd Griffiths <lloyd.griffiths@ons.gov.uk>
2016-08-12 06:57:04 +0100

    Decrypt configuration once pulled down from s3

--- scripts/codedeploy/application-start.sh
+++ scripts/codedeploy/application-start.sh
@@ -8,11 +8,12 @@ GIT_COMMIT=
 INSTANCE=$(curl -s http://instance-data/latest/meta-data/instance-id)
 CONFIG=$(aws --region $AWS_REGION ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE" "Name=key,Values=Configuration" --output text | awk '{print $5}')
 
-aws s3 cp s3://$CONFIG_BUCKET/performance-reporter/$CONFIG . || exit $?
+(aws s3 cp s3://$CONFIG_BUCKET/performance-reporter/$CONFIG.asc . && gpg --decrypt $CONFIG.asc > $CONFIG) || exit $?
 
 source $CONFIG && docker run -d                                \
   --env=AWS_BUCKET_NAME=$PERFORMANCE_BUCKET                    \
-  --env=GOOGLE_APPLICATION_CREDENTIALS=$GOOGLE_APP_CREDENTIALS \
+  --env=GOOGLE_ACCOUNT_ID=$GOOGLE_ACCOUNT_ID                   \
+  --env=GOOGLE_PRIVATE_KEY=$GOOGLE_PRIVATE_KEY                 \
   --env=GOOGLE_PROFILE_ID=$GOOGLE_PROFILE_ID                   \
   --env=INFLUXDB_URL=$INFLUXDB_URL                             \
   --env=PINGDOM_APPLICATION_KEY=$PINGDOM_APPLICATION_KEY       \
