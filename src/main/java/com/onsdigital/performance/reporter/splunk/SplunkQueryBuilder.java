fb517b5132bd56b45a0d50ec2fa8cef06a6316f1
CarlHembrough <carlhuk@gmail.com>
2016-08-22 12:57:24 +0100

    Added SplunkQuery builder to inject start and end dates into SplunkQueries.

---
+++ src/main/java/com/onsdigital/performance/reporter/splunk/SplunkQueryBuilder.java
@@ -0,0 +1,36 @@
+package com.onsdigital.performance.reporter.splunk;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class SplunkQueryBuilder {
+
+    private static SimpleDateFormat dateFormatter = new SimpleDateFormat("MM/dd/yyyy:HH:mm:ss");
+
+    public static String buildQuery(String input, Date startDate, Date endDate) {
+        String query = input;
+
+        // trim the known elements at the start of the query allowing us to insert the start / end times.
+        query = query.replaceFirst("^search", "")
+                .trim()
+                .replaceFirst("\\*", "")
+                .trim();
+
+        query = query.replaceFirst("earliest=\\d\\d/\\d\\d/\\d\\d\\d\\d:\\d\\d:\\d\\d:\\d\\d", "")
+                .trim();
+
+        query = query.replaceFirst("latest=\\d\\d/\\d\\d/\\d\\d\\d\\d:\\d\\d:\\d\\d:\\d\\d", "")
+                .trim();
+
+        String start = dateFormatter.format(startDate);
+        String end = dateFormatter.format(endDate);
+
+        query = String.format("search earliest=%s latest=%s %s", start, end, query);
+
+        return query;
+    }
+
+    public static String buildQuery(String input) {
+        return buildQuery(input, null, null);
+    }
+}
