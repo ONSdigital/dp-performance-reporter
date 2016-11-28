dae4cc31df716f9730bf0585016cce61b81bb21d
Carl Hembrough <carlhuk@gmail.com>
2016-11-28 14:07:34 +0000

    Refactor to allow any field to be compared as a percentage. Limiting public interface of the class by making internal methods private.

--- src/main/java/com/onsdigital/performance/reporter/google/PercentageComparison.java
+++ src/main/java/com/onsdigital/performance/reporter/google/PercentageComparison.java
@@ -26,42 +26,50 @@ import java.util.Map;
  */
 public class PercentageComparison implements CompositeMetricProvider {
 
+    private static final String ComparisonFieldKey = "comparisonField";
     private MetricProvider metricProvider;
 
     @Override
     public Metric getMetric(MetricDefinition metricDefinition) throws IOException {
 
-        Metric metricA = getInputMetric(metricDefinition, "filterA");
-        Metric metricB = getInputMetric(metricDefinition, "filterB");
+        Metric metricA = getInputMetric(metricDefinition, "comparisonValueA");
+        Metric metricB = getInputMetric(metricDefinition, "comparisonValueB");
 
         return createOutputMetric(metricA, metricB);
     }
 
     private Metric getInputMetric(MetricDefinition metricDefinition, String dimension) throws IOException {
         // Take the given metric definition and copy the query from it into two separate definitions.
-        MetricDefinition request = createDefinitionUsingFilter(dimension, metricDefinition);
+        MetricDefinition request = createDefinitionUsingFilter(metricDefinition, dimension);
 
         // Call out to Google analytics for the individual metric data to compare.
         return metricProvider.getMetric(request);
     }
 
-    static Metric createOutputMetric(Metric metricA, Metric metricB) {
+    private static Metric createOutputMetric(Metric metricA, Metric metricB) {
         // Create the result metric
         Metric metric = new Metric();
         metric.values = new ArrayList<>();
+
+        if (metric.columns != null)
         metric.columns = metricA.columns; // copy column headers
+
         metric.values = calculateMetricValues(metricA, metricB);
+
         return metric;
     }
 
-    static List<List<String>> calculateMetricValues(Metric metricA, Metric metricB) {
+    private static List<List<String>> calculateMetricValues(Metric metricA, Metric metricB) {
         List<List<String>> values = new ArrayList<>();
-        for (int rowIndex = 0; rowIndex < metricA.values.size(); rowIndex++) {
-            List<String> rowA = metricA.values.get(rowIndex);
-            List<String> rowB = metricB.values.get(rowIndex);
 
-            ArrayList<String> newRow = calculateResultRowValues(rowA, rowB);
-            values.add(newRow);
+        if (metricA != null && metricB != null) {
+            for (int rowIndex = 0; rowIndex < metricA.values.size(); rowIndex++) {
+                List<String> rowA = metricA.values.get(rowIndex);
+                List<String> rowB = metricB.values.get(rowIndex);
+
+                ArrayList<String> newRow = calculateResultRowValues(rowA, rowB);
+                values.add(newRow);
+            }
         }
         return values;
     }
@@ -73,7 +81,7 @@ public class PercentageComparison implements CompositeMetricProvider {
      * @param rowB - the second row to use values from
      * @return - the row of result values
      */
-    static ArrayList<String> calculateResultRowValues(List<String> rowA, List<String> rowB) {
+    private static ArrayList<String> calculateResultRowValues(List<String> rowA, List<String> rowB) {
         ArrayList<String> newRow = new ArrayList<>();
         newRow.add(rowA.get(0)); // add the date label in the first column.
 
@@ -98,17 +106,20 @@ public class PercentageComparison implements CompositeMetricProvider {
      * Create a new MetricDefinition, using the query from the given MetricDefinition, and also adding a dimension
      * value to the query as defined in the metadata of the sourceDefinition.
      *
-     * @param filterKey - The key of the filter in the meta map
      * @param sourceDefinition - The MetricDefinition to create the result from.
+     * @param filterKey - The key of the filter in the meta map
      * @return - The newly created MetricDefinition.
      */
-    static MetricDefinition createDefinitionUsingFilter(String filterKey, MetricDefinition sourceDefinition) {
+    private static MetricDefinition createDefinitionUsingFilter(MetricDefinition sourceDefinition, String filterKey) {
 
         if (sourceDefinition == null)
             throw new IllegalArgumentException("parameter MetricDefinition cannot be null.");
 
         if (sourceDefinition.meta == null)
             throw new IllegalArgumentException("parameter MetricDefinition cannot have null meta field.");
+
+        if (!sourceDefinition.meta.containsKey(ComparisonFieldKey))
+            throw new IllegalArgumentException("Could not find a value for 'comparisonField' in the meta data map of the sourceDefinition.");
 
         if (!sourceDefinition.meta.containsKey(filterKey))
             throw new IllegalArgumentException("Could not find the filter: " + filterKey + " in the meta data map of the sourceDefinition");
@@ -117,11 +128,13 @@ public class PercentageComparison implements CompositeMetricProvider {
 
         result.query = new HashMap<>();
 
-        for (Map.Entry<String, String> entry : sourceDefinition.query.entrySet()) {
-            result.query.put(entry.getKey(), entry.getValue());
+        if (sourceDefinition.query != null) {
+            for (Map.Entry<String, String> entry : sourceDefinition.query.entrySet()) {
+                result.query.put(entry.getKey(), entry.getValue());
+            }
         }
 
-        result.query.put("filters", sourceDefinition.meta.get(filterKey));
+        result.query.put(ComparisonFieldKey, sourceDefinition.meta.get(filterKey));
         return result;
     }
 
@@ -136,7 +149,7 @@ public class PercentageComparison implements CompositeMetricProvider {
     }
 
     // determine the percentage of a compared to b.
-    static double calculatePercentage(double a, double b) {
+    private static double calculatePercentage(double a, double b) {
         return (100 / b) * a;
     }
 }
